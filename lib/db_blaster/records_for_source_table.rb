6855ad217f1eef11d484b0ae00dc6b9a71929bd7
Perry Hertler <perry@hertler.org>
2021-07-29 11:32:43 -0600

    critical fixes

--- lib/db_blaster/records_for_source_table.rb
+++ lib/db_blaster/records_for_source_table.rb
@@ -4,7 +4,7 @@ module DbBlaster
   # Find records and yield them a `batch_size` at a time
   class RecordsForSourceTable
     include AvailableTables
-    attr_reader :source_table, :block_on_find
+    attr_reader :source_table, :block_on_find, :offset
 
     def initialize(source_table, &block)
       @source_table = source_table
@@ -12,11 +12,15 @@ module DbBlaster
       @offset = 0
     end
 
+    delegate :batch_size, :name, :last_published_updated_at, to: :source_table, prefix: true
+
     def self.find(source_table, &block)
       new(source_table, &block).find
     end
 
     def find
+      verify_source_table_name
+
       find_records_in_batches do |batch|
         block_on_find.call(batch.collect(&method(:filter_columns)))
       end
@@ -25,15 +29,12 @@ module DbBlaster
     private
 
     def find_records_in_batches
-      sql = "SELECT * FROM #{source_table_name} #{where} ORDER BY updated_at ASC LIMIT #{source_table.batch_size}"
-      offset = 0
-
       loop do
-        result = ActiveRecord::Base.connection.execute("#{sql} OFFSET #{offset}")
+        result = ActiveRecord::Base.connection.execute("#{select_sql} OFFSET #{offset}")
         yield(result)
-        break if result.count != source_table.batch_size
+        break if result.count != source_table_batch_size
 
-        offset += source_table.batch_size
+        @offset += source_table_batch_size
       end
     end
 
@@ -41,21 +42,23 @@ module DbBlaster
       selected_row.except(*source_table.ignored_columns)
     end
 
-    def source_table_name
-      raise invalid_source_table_message unless available_tables.include?(source_table.name)
-
-      source_table.name
+    def verify_source_table_name
+      raise invalid_source_table_message unless available_tables.include?(source_table_name)
     end
 
     def invalid_source_table_message
-      "source_table.name: '#{source_table.name}' does not exist!"
+      "source_table.name: '#{source_table_name}' does not exist!"
+    end
+
+    def select_sql
+      "SELECT * FROM #{source_table_name} #{where} ORDER BY updated_at ASC LIMIT #{source_table_batch_size}"
     end
 
     def where
-      return '' unless source_table.last_published_updated_at
+      return '' unless source_table_last_published_updated_at
 
       ActiveRecord::Base.sanitize_sql_for_conditions(
-        ['WHERE updated_at > :updated_at', { updated_at: source_table.last_published_updated_at.to_s(:db) }]
+        ['WHERE updated_at > :updated_at', { updated_at: source_table_last_published_updated_at.to_s(:db) }]
       )
     end
   end
