f36d0b3b9a504d20bb6c3a67d8ebcbaf2ab6823a
Perry Hertler <perry@hertler.org>
2021-07-29 10:34:29 -0600

    rubofix

--- lib/db_blaster/available_tables.rb
+++ lib/db_blaster/available_tables.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 module DbBlaster
+  # Finds all tables in current database and removes the "system tables"
   module AvailableTables
-    SYSTEM_TABLES = %w[schema_migrations ar_internal_metadata]
+    SYSTEM_TABLES = %w[schema_migrations ar_internal_metadata].freeze
 
     def available_tables
       @available_tables ||= ActiveRecord::Base.connection.tables - SYSTEM_TABLES
