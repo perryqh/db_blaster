53c1e14e73e9c16ff89abae8c3f1a32eee0a6f20
Perry Hertler <perry@hertler.org>
2021-09-01 10:52:50 -0600

    updating example config

--- lib/generators/db_blaster/install/templates/db_blaster_config.rb
+++ lib/generators/db_blaster/install/templates/db_blaster_config.rb
@@ -2,20 +2,37 @@
 
 # Either `sns_topic` or `s3_bucket` must be set
 DbBlaster.configure do |config|
-  # SNS topic to receive database changes
+  # SNS topic to receive database changes. Either `sns_topic` or `s3_bucket` must be set
   config.sns_topic = 'the-topic'
+  config.aws_access_key = 'access-key'
+  config.aws_access_secret = 'secret'
+  config.aws_region = 'region'
+
+  # Optional
+  # Applicable only when `sns_topic` is set
+  # Extra [SNS message_attributes](https://docs.aws.amazon.com/sns/latest/dg/sns-message-attributes.html)
+  # Attributes set here will be included in every published message
+  # config.extra_sns_message_attributes = {'infra_id' => {data_type: 'String', value: '061'}}
+
+  # S3 bucket where JSON will be pushed. Either `sns_topic` or `s3_bucket` must be set
   # config.s3_bucket = 'bucket-name'
 
-  # the S3 key path. The following values will get substituted:
+  # Optional
+  # Applicable only when `s3_bucket` is set
+  # The S3 key path. The following values will get substituted:
   # <batch_timestamp> - a timestamp signifying the beginning of the batch processing
   # <timestamp> - the current time
   # <table_name> - the name of the table associated with the S3 body
   # <uuid> - a universal identifier
-  config.s3_key_path = '<batch_timestamp>/kcp-api/001/<table_name>/<uuid>.json'
+  # config.s3_key = '<batch_timestamp>/kcp-api/001/<table_name>/<uuid>.json'
 
-  config.aws_access_key = 'access-key'
-  config.aws_access_secret = 'secret'
-  config.aws_region = 'region'
+  # Optional
+  # Applicable only when `s3_bucket' is set
+  # Extra meta values sent along with each payload
+  # example: config.s3_meta = {'infra_id' => '061'}
+  # The resulting JSON:
+  # {"meta" : {"infra_id" : "061", "src_app" : "kcp-api", "src_table" : "the-table"}, "records" : [] }
+  # config.s3_meta = {'infra_id' => '061'}
 
   # Optional
   # db_blaster will select and then publish `batch_size` rows at a time
@@ -25,11 +42,6 @@ DbBlaster.configure do |config|
   # db_blaster will publish no messages larger than this value
   # Default value is 256
   # config.max_message_size_in_kilobytes = 256
-
-  # Optional
-  # Extra [SNS message_attributes](https://docs.aws.amazon.com/sns/latest/dg/sns-message-attributes.html)
-  # Attributes set here will be included in every published message
-  # config.extra_sns_message_attributes = {'infra_id' => {data_type: 'String', value: '061'}}
 
   # Global list of column names not to include in published SNS messages
   # example: config.ignored_column_names = ['email', 'phone_number']
